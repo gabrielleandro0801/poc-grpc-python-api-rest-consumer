cee0a8876e6c49cc7c56e6a85b76ee5b05e0ec8a
Gabriel Leandro <gabrielleandro0801@gmail.com>
2022-04-21 18:52:52 -0300

    Preparing DB Connection

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from typing import Any
 from flask import Flask
 from flask_restful import Api
 
+import src.infrastructure.database.connection.db_connection as database
 from src import grpc_configuration
 from src.routes import user_routes
 
@@ -10,6 +11,7 @@ app: Flask = Flask(__name__)
 api: Api = Api(app)
 
 stub: Any = grpc_configuration.configure_client()
+# database.start_connection(app)
 
 api = user_routes.add(api, stub)
 
